80d9d9d4c81ba75debc9140f9b28e9bfc25e281b
Amanda Mahendra <mahendraamanda0@gmail.com>
2025-10-04 14:59:15 +0700

    Kemanjuan belajar 2

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+    extend: {
+        fontFamily: {
+            inter: ['Inter'],
+        },
+        colors: {
+            cokelat: '#F5EFE6',
+        }
+    },
+    },
+    plugins: [],
+}
